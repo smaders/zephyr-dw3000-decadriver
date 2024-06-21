c828ee9bf205b8641d375cda60692d96fb29e2d0
Benjamin Vincent <benjamin@smade.io>
2024-06-21 09:49:28 +0200

    ♻️:rename fini by uninit

--- zephyr/platform/dw3000_hw.h
+++ zephyr/platform/dw3000_hw.h
@@ -5,7 +5,7 @@ typedef void (* dw3000_isr_cb)(void);
 
 int dw3000_hw_init(void);
 int dw3000_hw_init_interrupt(void);
-void dw3000_hw_fini(void);
+void dw3000_hw_uninit(void);
 void dw3000_hw_reset(void);
 void dw3000_hw_wakeup(void);
 void dw3000_hw_wakeup_pin_low(void);
